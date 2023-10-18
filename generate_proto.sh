28c67b1de214eb2155a159803105eabe9cb44644
makshark <vitalii.makovskyi@gmail.com>
2023-10-18 21:13:45 +0300

    Fix problem with initial lib folder creation

--- generate_proto.sh
+++ generate_proto.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+mkdir -p ./client/lib
+mkdir -p ./server/lib
+
 grpc_tools_ruby_protoc -I ./protos --ruby_out=./client/lib --grpc_out=./client/lib ./protos/messages.proto
 grpc_tools_ruby_protoc -I ./protos --ruby_out=./server/lib --grpc_out=./server/lib ./protos/messages.proto
